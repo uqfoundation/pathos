9c908473405648d20cf31f74dba08a61ce66c1c0
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-01-02 15:37:16 +0000

    updated copyright to 2015
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pathos@728 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- pathos/pp.py
+++ pathos/pp.py
@@ -6,7 +6,7 @@
 #
 # Forked by: Mike McKerns (April 2008)
 # Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
-# Copyright (c) 2008-2014 California Institute of Technology.
+# Copyright (c) 2008-2015 California Institute of Technology.
 # License: 3-clause BSD.  The full license text is available at:
 #  - http://trac.mystic.cacr.caltech.edu/project/pathos/browser/pathos/LICENSE
 #
