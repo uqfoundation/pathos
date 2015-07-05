8b3398b35a37f0bddba4d386c1e80208316c2cde
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-07-05 05:40:43 +0000

    add missing API methods to SerialPool
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pathos@797 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- pathos/serial.py
+++ pathos/serial.py
@@ -62,21 +62,64 @@ class SerialPool(AbstractWorkerPool):
     """
 Mapper that leverages standard (i.e. serial) python maps.
     """
+    # interface (no __init__)
+    _exiting = False
+
     def map(self, f, *args, **kwds):
        #AbstractWorkerPool._AbstractWorkerPool__map(self, f, *args, **kwds)
+        if self._exiting: self._is_alive()
         return _map(f, *args)#, **kwds)
     map.__doc__ = AbstractWorkerPool.map.__doc__
     def imap(self, f, *args, **kwds):
        #AbstractWorkerPool._AbstractWorkerPool__imap(self, f, *args, **kwds)
+        if self._exiting: self._is_alive()
         return _imap(f, *args)#, **kwds)
     imap.__doc__ = AbstractWorkerPool.imap.__doc__
     ########################################################################
     # PIPES
     def pipe(self, f, *args, **kwds):
        #AbstractWorkerPool._AbstractWorkerPool__pipe(self, f, *args, **kwds)
+        if self._exiting: self._is_alive()
         return _apply(f, args, kwds)
     pipe.__doc__ = AbstractWorkerPool.pipe.__doc__
     #XXX: generator/yield provides simple ipipe? apipe? what about coroutines?
+    ########################################################################
+    def restart(self, force=False):
+        "restart a closed pool"
+        if not force and not self._exiting: self._is_alive(negate=True)
+        # 'clear' and 'serve'
+        if self._exiting: # i.e. is destroyed
+            self.clear()
+        return
+    def _is_alive(self, negate=False, run=True):
+        RUN,CLOSE,TERMINATE = 0,1,2
+        pool = lambda :None
+        pool._state = RUN if negate else CLOSE
+        if negate and run: # throw error if alive (exiting=True)
+            assert pool._state != RUN
+        elif negate: # throw error if alive (exiting=True)
+            assert pool._state in (CLOSE, TERMINATE)
+        else: # throw error if not alive (exiting=False)
+            assert pool._state == RUN
+    def close(self):
+        "close the pool to any new jobs"
+        self._exiting = True
+        return
+    def terminate(self):
+        "a more abrupt close"
+        self.close()
+        self.join()
+        return
+    def join(self):
+        "cleanup the closed worker processes"
+        if not self._exiting: 
+            self._is_alive(negate=True, run=False)
+        self._exiting = True
+        return
+    def clear(self):
+        """hard restart"""
+        self._exiting = False
+        return
     ########################################################################
     # interface
     __get_nodes = __get_nodes__
