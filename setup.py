0e3c952db6f773382b406d9de9d8d358caac6cc0
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2017-02-01 19:45:44 +0000

    nudging past dill-0.2.7
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pathos@958 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -211,7 +211,7 @@ Requirements
 `pathos` requires::
 
     - python2, version >= 2.6  *or*  python3, version >= 3.1
-    - dill, version >= 0.2.5
+    - dill, version >= 0.2.6
     - pox, version >= 0.2.3
     - ppft, version >= 1.6.4.5
     - multiprocess, version >= 0.70.4
@@ -369,7 +369,7 @@ except Exception:
 
 # add dependencies
 ppft_version = '>=1.6.4.5'
-dill_version = '>=0.2.5'
+dill_version = '>=0.2.6'
 pox_version = '>=0.2.3'
 mp_version = '>=0.70.4' if version_info >= (2,6) else '>=0.52.0'
 pyina_version = '>=0.2.0.dev0'
