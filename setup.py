b97c508bf939cccb36e3e56019131c764e5a3447
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2018-06-21 01:45:15 +0000

    bumping past pox-0.2.4
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pathos@1060 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -211,7 +211,7 @@ Requirements
 
     - ``python``, **version >= 2.6** or **version >= 3.1**, or ``pypy``
     - ``dill``, **version >= 0.2.7.1**
-    - ``pox``, **version >= 0.2.3**
+    - ``pox``, **version >= 0.2.4**
     - ``ppft``, **version >= 1.6.4.7**
     - ``multiprocess``, **version >= 0.70.5**
 
@@ -367,7 +367,7 @@ except Exception:
 # add dependencies
 ppft_version = '>=1.6.4.7'
 dill_version = '>=0.2.7.1'
-pox_version = '>=0.2.3'
+pox_version = '>=0.2.4'
 mp_version = '>=0.70.5' if version_info >= (2,6) else '>=0.52.0'
 pyina_version = '>=0.2.0.dev0'
 rpyc_version = '>=3.0.6'
