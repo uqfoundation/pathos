3fd7ed1dd8912b0bbc98c785aeff1ef5b5f4e3d4
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2019-01-09 04:57:21 +0000

    utilize extras_require for optional dependencies
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/pathos@1111 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -220,6 +220,7 @@ Optional requirements:
     - ``setuptools``, **version >= 0.6**
     - ``pyina``, **version >= 0.2.0**
     - ``rpyc``, **version >= 3.0.6**
+    - ``mystic``, **version >= 0.3.2**
 
 
 More Information
