bf4f293521ca93292f676d5fe4442075ccdf518a
mmckerns <mmckerns@caltech.edu>
2020-06-16 12:52:02 -0400

    bumping to pyina-0.2.3

--- setup.py
+++ setup.py
@@ -225,7 +225,7 @@ Requirements
 Optional requirements:
 
     - ``setuptools``, **version >= 0.6**
-    - ``pyina``, **version >= 0.2.2**
+    - ``pyina``, **version >= 0.2.3**
     - ``rpyc``, **version >= 3.0.6**
     - ``mystic``, **version >= 0.3.5**
 
@@ -377,7 +377,7 @@ ppft_version = '>=1.6.6.2'
 dill_version = '>=0.3.2'
 pox_version = '>=0.2.8'
 mp_version = '>=0.70.10' if version_info >= (2,6) else '>=0.52.0'
-pyina_version = '>=0.2.2'
+pyina_version = '>=0.2.3'
 rpyc_version = '>=3.0.6'
 deps = [ppft_version, dill_version, pox_version]
 if mp_version:
