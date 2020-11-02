7c3bbbd0bcab1a7cc9729fd271707bbe6087f9bf
mmckerns <mmckerns@caltech.edu>
2020-11-02 18:05:44 -0500

    bumping to multiprocess-0.70.11

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ Requirements
     - ``dill``, **version >= 0.3.3**
     - ``pox``, **version >= 0.2.9**
     - ``ppft``, **version >= 1.6.6.3**
-    - ``multiprocess``, **version >= 0.70.10**
+    - ``multiprocess``, **version >= 0.70.11**
 
 Optional requirements:
 
@@ -368,7 +368,7 @@ try: #NOTE: odd... if processing is installed, *don't* install multiprocess
         mp_version = ''
     else: raise AttributeError('multiprocess')
 except Exception:
-    mp_version = '>=0.70.10' # 0.70a1 py25-py33, 0.52 on py25, None on py34
+    mp_version = '>=0.70.11' # 0.70a1 py25-py33, 0.52 on py25, None on py34
     processing_version = ''
 '''
 
@@ -376,7 +376,7 @@ except Exception:
 ppft_version = '>=1.6.6.3'
 dill_version = '>=0.3.3'
 pox_version = '>=0.2.9'
-mp_version = '>=0.70.10' if version_info >= (2,6) else '>=0.52.0'
+mp_version = '>=0.70.11' if version_info >= (2,6) else '>=0.52.0'
 pyina_version = '>=0.2.3'
 rpyc_version = '>=3.0.6'
 deps = [ppft_version, dill_version, pox_version]
