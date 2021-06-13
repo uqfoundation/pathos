340b78c474d9e0a93e6d5fb00a540c1bef7133e5
mmckerns <mmckerns@caltech.edu>
2021-06-13 13:02:22 -0400

    bumping to pox-0.3.0

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ Requirements
 
     - ``python``, **version == 2.7** or **version >= 3.6**, or ``pypy``
     - ``dill``, **version >= 0.3.3**
-    - ``pox``, **version >= 0.2.9**
+    - ``pox``, **version >= 0.3.0**
     - ``ppft``, **version >= 1.6.6.3**
     - ``multiprocess``, **version >= 0.70.11.1**
 
@@ -380,7 +380,7 @@ except Exception:
 # add dependencies
 ppft_version = '>=1.6.6.3'
 dill_version = '>=0.3.3'
-pox_version = '>=0.2.9'
+pox_version = '>=0.3.0'
 mp_version = '>=0.70.11.1' if version_info >= (2,6) else '>=0.52.0'
 pyina_version = '>=0.2.4'
 rpyc_version = '>=3.0.6'
