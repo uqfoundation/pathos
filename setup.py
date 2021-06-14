9e225b9fc3ba0e16c83b4a16c3aa2c4ccf649264
mmckerns <mmckerns@caltech.edu>
2021-06-14 09:17:38 -0400

    tag pathos-0.2.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if unsupported:
     raise ValueError(unsupported)
 
 # set version numbers
-stable_version = '0.2.7'
+stable_version = '0.2.8'
 target_version = '0.2.8'
 is_release = stable_version == target_version
 
