71ad7d5ef640fdea79851c49f9d565c7c692d299
mmckerns <mmckerns@caltech.edu>
2021-10-05 11:54:14 -0400

    formal support for python 3.10

--- setup.py
+++ setup.py
@@ -352,6 +352,7 @@ setup(name="pathos",
                    'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
                    'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
                    'Topic :: Scientific/Engineering',
                    'Topic :: Software Development'],
 
