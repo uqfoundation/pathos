6d9b8cb1717a36eb72cb8c328f6c12b2326413ac
mmckerns <mmckerns@caltech.edu>
2022-01-01 13:32:51 -0500

    update copyright for 2022

--- pathos/server.py
+++ pathos/server.py
@@ -13,7 +13,7 @@
 # Forked by: Mike McKerns (January 2004)
 # Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
 # Copyright (c) 2004-2016 California Institute of Technology.
-# Copyright (c) 2016-2021 The Uncertainty Quantification Foundation.
+# Copyright (c) 2016-2022 The Uncertainty Quantification Foundation.
 # License: 3-clause BSD.  The full license text is available at:
 #  - https://github.com/uqfoundation/pathos/blob/master/LICENSE
 """
