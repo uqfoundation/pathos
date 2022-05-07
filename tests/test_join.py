931398a1929c05e1f12c8937a67a97c67a459ed3
mmckerns <mmckerns@caltech.edu>
2022-05-07 11:12:20 -0400

    remove odd 3.7.7 workaround for travis

--- tests/test_join.py
+++ tests/test_join.py
@@ -14,8 +14,8 @@ from pathos.helpers import cpu_count
 
 import sys
 import dill
-P33 = (sys.hexversion >= 0x30300f0) #FIXME: below oddity due to travis
-P37 = (sys.hexversion >= 0x30700f0) and sys.version_info[:3] != (3,7,7)
+P33 = (sys.hexversion >= 0x30300f0)
+P37 = (sys.hexversion >= 0x30700f0)
 PYPY38 = (sys.hexversion >= 0x30800f0) and dill._dill.IS_PYPY
 PoolClosedError = ValueError if P33 else AssertionError
 PoolRunningError = ValueError if P37 else AssertionError
