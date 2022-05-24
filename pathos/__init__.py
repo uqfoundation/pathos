db2acb55f8f41f73cf36534c34d8dbf26fe1179d
mmckerns <mmckerns@caltech.edu>
2022-05-24 07:03:26 -0400

    bumping past pathos-0.2.9

--- pathos/__init__.py
+++ pathos/__init__.py
@@ -8,7 +8,7 @@
 #  - https://github.com/uqfoundation/pathos/blob/master/LICENSE
 
 # author, version, license, and long description
-__version__ = '0.2.9'
+__version__ = '0.3.0.dev0'
 __author__ = 'Mike McKerns'
 
 __doc__ = """
