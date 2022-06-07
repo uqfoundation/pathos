bcd29b549f4458bccdfa15416ad95f7a08f9f1f0
mmckerns <mmckerns@caltech.edu>
2022-06-07 14:28:46 -0400

    print once per test success

--- tests/__main__.py
+++ tests/__main__.py
@@ -26,6 +26,6 @@ if __name__ == '__main__':
     for test in tests:
         p = sp.Popen([python, test], shell=shell).wait()
         if not p:
-            print('.', end='')
+            print('.', end='', flush=True)
     print('')
 
