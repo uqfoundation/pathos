0f053c4114bc98f4f22acfc0e0d709bd918acd76
mmckerns <mmckerns@caltech.edu>
2022-10-23 22:57:34 -0400

    tag: pathos-0.3.0

--- setup.py
+++ setup.py
@@ -88,10 +88,10 @@ class BinaryDistribution(Distribution):
         return True
 
 # define dependencies
-ppft_version = 'ppft>=1.7.6.5'
-dill_version = 'dill>=0.3.5.1'
-pox_version = 'pox>=0.3.1'
-mp_version = 'multiprocess>=0.70.13'
+ppft_version = 'ppft>=1.7.6.6'
+dill_version = 'dill>=0.3.6'
+pox_version = 'pox>=0.3.2'
+mp_version = 'multiprocess>=0.70.14'
 pyina_version = 'pyina>=0.2.6'
 mystic_version = 'mystic>=0.3.9'
 # add dependencies
