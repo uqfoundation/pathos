a0eebf7e3632831db03ed2c0f46b5180d297b187
mmckerns <mmckerns@caltech.edu>
2023-08-21 19:12:49 -0400

    example of nested parallel using pathos.maps

---
+++ examples/nested2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 2015-2016 California Institute of Technology.
+# Copyright (c) 2016-2023 The Uncertainty Quantification Foundation.
+# License: 3-clause BSD.  The full license text is available at:
+#  - https://github.com/uqfoundation/pathos/blob/master/LICENSE
+
+def g(x):
+  import random
+  return int(x * random.random())
+
+def h(x):
+  return sum(tmap(g, x))
+
+def f(x,y):
+  return x*y
+
+x = range(10)
+y = range(5)
+
+
+if __name__ == '__main__':
+    from pathos.helpers import freeze_support, shutdown
+    freeze_support()
+
+    from pathos.pools import ProcessPool, ThreadPool
+    from pathos.maps import Map, Amap
+    amap = Amap(ProcessPool)#, close=True, join=True, clear=True)
+    tmap = Map(ThreadPool, close=True, join=True, clear=True)
+
+    print(amap(f, [h(x),h(x),h(x),h(x),h(x)], y).get())
+
+    def _f(m, g, x, y):
+      return sum(m(g,x))*y
+
+    print(amap(_f, [tmap]*len(y), [g]*len(y), [x]*len(y), y).get())
+
+    from math import sin, cos
+
+    print(amap(tmap, [sin,cos], [x,x]).get())
+
+    shutdown()
+
+# EOF
