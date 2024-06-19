24c5392a31704db434ab826324b2b04ce00e2ae6
Mike McKerns <mmckerns@caltech.edu>
2024-06-19 17:04:48 -0400

    bump requirements in setup, docs to rtfd 10.27.0 (#289)

---
+++ examples/map_pool.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Author: Mike McKerns (mmckerns @uqfoundation)
+# Copyright (c) 2024 The Uncertainty Quantification Foundation.
+# License: 3-clause BSD.  The full license text is available at:
+#  - https://github.com/uqfoundation/pathos/blob/master/LICENSE
+
+from pathos.maps import Imap
+from pathos.pools import ProcessPool
+squared = lambda x:x*x
+
+# serial map (in-line for loop)
+print("list(map(squared, range(4))): %s" % list(map(squared, range(4))))
+
+# pathos serial map
+_map = Imap()
+print("list(Imap()(squared, range(4))): %s" % list(_map(squared, range(4))))
+
+# pathos process-parallel map
+_map = Imap(ProcessPool)
+print("list(Imap(ProcessPool)(squared, range(4))): %s" % list(_map(squared, range(4))))
+
+# pathos pool-based parallel map
+pool = ProcessPool()
+print("list(ProcessPool().imap(squared, range(4))): %s" % list(pool.imap(squared, range(4))))
+
+# pathos asynchronous parallel map
+result = pool.amap(squared, range(4))
+print("ProcessPool().amap(squared, range(4)).get(): %s" % result.get())
+
+# pathos thread-parallel map
+from pathos.pools import ThreadPool
+tpool = ThreadPool()
+print("list(ThreadPool().imap(squared, range(4))): %s" % list(tpool.imap(squared, range(4))))
